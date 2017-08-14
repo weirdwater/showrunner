d45d2c0ce4c9295b8d5fed721970cfdf1d3a2a8f
Arjo Bruijnes <arjo@weirdwater.net>
2017-08-14 12:14:37 +0200

    Add ShowsList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import {DataService} from './data.service';
 import {HttpClientModule} from '@angular/common/http';
 import { UserControlsComponent } from './user-controls/user-controls.component';
 import { ModalComponent } from './modal/modal.component';
+import { ShowsListComponent } from './shows-list/shows-list.component';
+import {NumberOfEpisodesPipe} from './pipes/number-of-episodes.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserControlsComponent,
+    ShowsListComponent,
+    NumberOfEpisodesPipe,
     ModalComponent
   ],
   imports: [
