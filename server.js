3175d81b2021d18916c58e2bbf3491657cc18898
Arjo Bruijnes <arjo@weirdwater.net>
2017-08-15 20:56:48 +0200

    Allow cross origin requests

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const api = require('./server/routes/api')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.use('/api', api)
