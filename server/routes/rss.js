597a265dd74b264e776d2b3b1e742d4fd2fa93d6
Arjo Bruijnes <arjo@weirdwater.net>
2017-08-18 11:52:44 +0200

    Replace feed module for rss, generate iTunes rss

--- server/routes/rss.js
+++ server/routes/rss.js
@@ -1,30 +1,39 @@
 const express = require('express')
 const router = express.Router()
 const showRepo = require('../showRepository')
-const Feed = require('feed')
+const RSS = require('rss')
 
 function newShowFeed(show, req) {
-  const feed = new Feed({
+  const { appUrl } = req.app.locals
+  const feed = new RSS({
     title: show.title,
     description: show.description,
-    link: show.link,
+    feed_url: `${appUrl}/rss/shows/${show.slug}`,
+    site_url: `${appUrl}/shows/${show.slug}`,
     copyright: show.copyright,
-    image: `${req.app.locals.appUrl}/assets/${show.art}`,
-    author: {
-      name: 'John Doe',
-      email: 'john@doe.com',
-      link: 'https://doe.com'
-    }
-  })
-
-
-  show.episodes.forEach(episode => {
-    feed.addItem({
-      title:          episode.title,
-      link:           episode.link,
-      description:    episode.description,
-      date:           episode.dateCreated
-    })
+    language: show.language,
+    pubDate: show.dateCreated,
+    custom_namespaces: {
+      itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd'
+    },
+    custom_elements: [
+      {'itunes:author': show.author},
+      {'itunes:summary': show.description},
+      {'itunes:owner': [
+        {"itunes:name": 'Arjo Bruijnes'},
+        {"itunes:email": 'arjo@weirdwater.net'}
+      ]},
+      {'itunes:image': {
+        _attr: {
+          href: `${appUrl}/assets/${show.art}`
+        }
+      }},
+      {'itunes:category': {
+        _attr: {
+          text: show.category
+        }
+      }}
+    ]
   })
 
   return feed
@@ -36,7 +45,7 @@ router.get('/shows/:slug', (req, res, next) => {
     .then(show => {
       const feed = newShowFeed(show, req)
       res.set('Content-Type', 'text/xml')
-      res.send(feed.rss2())
+      res.send(feed.xml())
     })
     .catch(next)
 })
