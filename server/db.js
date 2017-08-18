bbe98261897149eecee43534226837d6d038a96a
Arjo Bruijnes <arjo@weirdwater.net>
2017-08-18 18:06:40 +0200

    Connect to mongo using docker link

--- server/db.js
+++ server/db.js
@@ -2,8 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 
 module.exports = {
   connection: (closure) => {
-    const host = process.env.HOST_DOMAIN || 'localhost'
-    return MongoClient.connect(`mongodb://${host}:27017/showrunner`, (err, db) => {
+    return MongoClient.connect(`mongodb://mongo:27017/showrunner`, (err, db) => {
       if (err) return console.log(err)
 
       closure(db)
